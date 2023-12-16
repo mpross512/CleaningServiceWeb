05fee57b3d060691c4a3e43eaa755544aa5da7ed
Michael Ross <mpross512@gmail.com>
2023-12-15 20:38:38 -0600

    Added headers on the about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,12 @@
 function About() {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1 class="text-3xl font-bold">About Us</h1>
+
+            <h2 class="text-2xl font-bold">Mike Ross</h2>
+            <h3 class=" text-base text-blue-500 italic">Owner and Founder of Beyond Clean, LLC</h3>
+
+
         </div>
     )
 }
--- src/pages/quote/Quote.jsx
+++ src/pages/quote/Quote.jsx
@@ -2,6 +2,17 @@ function Quote() {
     return (
         <>
             <h1>Get a Quote</h1>
+            <form>
+                <label>
+                    Name
+                    <input placeholder="Name"></input>
+                </label>
+                <label>
+                    Phone Number
+                    <input type="phone"></input>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
